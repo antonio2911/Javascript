05fd2526fd9c9535bec160987261c618e9f1f722
antonio2911 <antoniojrpardim@gmail.com>
2020-11-02 19:39:54 -0300

    template string

---
+++ fundamentos/template-string.js
@@ -0,0 +1,22 @@
+const nome = 'Rebeca';
+const concatenacao = 'ola ' + nome + '!';
+console.log(concatenacao);
+const template = `
+olá
+${nome}!` //com a crase pode concatenar
+    //quebrando linhas
+console.log(template)
+    //expresões
+console.log(`1 + 1 = ${1 + 1}`);
+const up = texto => texto.toUpperCase();
+console.log(up('Antonio Junio Pardim'));
+// const nomeText = ' ';
+
+function UpperCase(nomeText, number) {
+    return nomeText.toUpperCase() + parseFloat(number).toFixed(2);
+}
+console.log(UpperCase('Drielle Cristina'));
+console.log(`Ola ${UpperCase('Antonio')} !`);
+console.log(UpperCase('Leonardo Santo Damasceno'));
+console.log(UpperCase('Ola sou o Jonathan ', 3.7874))
+console.log(UpperCase('Ola sou o Jonathan ', 3.7874))
