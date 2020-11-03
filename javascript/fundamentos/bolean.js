f1e3675d0546f334c99fdff73bbf917e2d486fcc
antonio2911 <antoniojrpardim@gmail.com>
2020-11-02 22:15:52 -0300

    novo arquivo boolean

---
+++ javascript/fundamentos/bolean.js
@@ -0,0 +1,9 @@
+let testebolean = false;
+console.log(testebolean)
+testebolean = true
+console.log(testebolean)
+testebolean = 1
+console.log(!!testebolean)
+console.log(!testebolean) //negar a declaração
+console.log(!false)
+console.log(!!false)
